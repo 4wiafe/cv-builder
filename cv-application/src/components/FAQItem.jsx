31294e2610c870538a2b3975ba96e3bbbe709b52
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-01-21 14:27:20 +0000

    Add FAQItem component

--- cv-application/src/components/FAQItem.jsx
+++ cv-application/src/components/FAQItem.jsx
@@ -1,3 +1,29 @@
 import ToggleButon from "./ToggleButton";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+
+export default function FAQItem({ question, answer, isOpen, onToggle }) {
+  return (
+    <div className={`faq-item ${isOpen ? "open" : ""}`}>
+      <ToggleButon
+        isOn={isOpen}
+        onToggle={onToggle}
+        onIcon={<FontAwesomeIcon icon={faChevronUp} />}
+        offIcon={<FontAwesomeIcon icon={faChevronDown} />}
+        buttonClass="faq-toggle"
+        ariaLabel="Toggle FAQ answer"
+      >
+        <div className="question">
+          <h3>{question}</h3>
+        </div>
+      </ToggleButon>
+      {
+        isOpen && (
+          <div className="faq-answer">
+            <p>{answer}</p>
+          </div>
+        )
+      }
+    </div>
+  );
+}
