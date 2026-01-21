7ee144524f2d003e78b610d8754f1562eee5bba4
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-01-21 13:42:15 +0000

    Import toggle button and icons

---
+++ cv-application/src/components/FAQItem.jsx
@@ -0,0 +1,3 @@
+import ToggleButon from "./ToggleButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
