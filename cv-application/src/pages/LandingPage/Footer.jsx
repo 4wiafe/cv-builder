ded9123d42fa61a86d4172c32c38a628fa87d201
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-02-06 00:47:43 +0000

    Add social media icons

--- cv-application/src/pages/LandingPage/Footer.jsx
+++ cv-application/src/pages/LandingPage/Footer.jsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faLinkedin, faXTwitter, faTiktok } from "@fortawesome/free-brands-svg-icons";
+
 export default function Footer() {
   return (
     <footer>
@@ -13,6 +16,13 @@ export default function Footer() {
         <h3 className="qcv">Quick CV</h3>
         <a href="#">About us</a>
         <a href="#">Reviews</a>
+      </div>
+
+      <div className="socials-container">
+        <span><FontAwesomeIcon icon={faFacebook} /></span>
+        <span><FontAwesomeIcon icon={faLinkedin} /></span>
+        <span><FontAwesomeIcon icon={faXTwitter} /></span>
+        <span><FontAwesomeIcon icon={faTiktok} /></span>
       </div>
     </footer>
   );
